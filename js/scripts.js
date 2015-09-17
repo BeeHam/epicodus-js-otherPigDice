5279c63fdca8affc043cc70e7516621cf2a3a864
brigham thompson & clinton herrick <student@epicodus.com>
2015-09-17 16:01:50 -0700

    add what dice roll value is and dice roll image

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
-var rollDice = function() {
-  var randomNumber = Math.round(Math.random() * 5 + 1)
-  return randomNumber
-}
-
 var playerOneScore = 0
 var playerTwoScore = 0
 var turnScore = 0
 var counter = 0
 var scoreToWin = 100
 
-var rollPigDice = function() {
-  var diceRoll = rollDice()
+
+var rollDice = function() {
+  var randomNumber = Math.floor(Math.random() * 6) + 1;
+  return randomNumber
+}
+
+
+var rollPigDice = function(diceRoll) {
+  // var diceRoll = rollDice()
   if (diceRoll === 1) {
     turnScore = 0
     counter += 1
@@ -44,9 +46,25 @@ var addPlayerTwoScore = function() {
 
 $(document).ready(function() {
   $("button#roll-dice").click(function() {
-    var newScore = rollPigDice()
-    $(".scores h3#turn-score").text(newScore)
-    // $(".scores h2#dice-roll").text(diceRoll)
+    var crap = rollDice();
+    var newScore = rollPigDice(crap)
+
+    $(".scores h3#turn-score").text("Running total: " + newScore)
+
+    if (crap === 1) {
+      $("#dice-roll").replaceWith('<img src="image/d-1.png" id="dice-roll">')
+    } else if (crap === 2) {
+        $("#dice-roll").replaceWith('<img src="image/d-2.png" id="dice-roll">')
+    } else if (crap === 3) {
+        $("#dice-roll").replaceWith('<img src="image/d-3.png" id="dice-roll">')
+    } else if (crap === 4) {
+        $("#dice-roll").replaceWith('<img src="image/d-4.png" id="dice-roll">')
+    } else if (crap === 5) {
+        $("#dice-roll").replaceWith('<img src="image/d-5.png" id="dice-roll">')
+    } else {
+        $("#dice-roll").replaceWith('<img src="image/d-6.png" id="dice-roll">')
+    }
+
     if (counter % 2 !== 0 && newScore === 0) {
       $("h2#whose-turn").text("Player 2 is on a roll!")
     } else if (counter % 2 === 0 && newScore === 0){
@@ -64,7 +82,7 @@ $(document).ready(function() {
      } else {
        addPlayerTwoScore()
        $("h2#whose-turn").text("Player 1 is on a roll!")
-       $(".scores h4#player2-total-score").text("Player 2 total score: " + playerTwoScore)
+       $(".scores h4#player2-total-score").text("Player 2 total score: "  + playerTwoScore)
      }
    })
 
